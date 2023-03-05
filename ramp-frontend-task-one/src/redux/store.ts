ae88cd753b721a8f5e4420fdba58f24838626f2b
Chuck Joshua Mbanaso <chuksjoshuaa@gmail.com>
2023-03-05 15:34:35 +0100

    WIP: Disable Redux Logger

--- ramp-frontend-task-one/src/redux/store.ts
+++ ramp-frontend-task-one/src/redux/store.ts
@@ -1,12 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import transactionReducer from "./features/transaction/transactionSlice";
-import logger from "redux-logger";
+// import logger from "redux-logger";
 
 export const store = configureStore({
   reducer: {
     data: transactionReducer,
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+  // middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
